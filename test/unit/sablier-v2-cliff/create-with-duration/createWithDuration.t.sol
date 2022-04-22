46234e3820728f71bba63a614d1e9047283b8a51
Andrei Vlad <andreivladbrg@gmail.com>
2022-04-22 16:50:53 +0200

    style: unnecessary comment

--- test/unit/sablier-v2-cliff/create-with-duration/createWithDuration.t.sol
+++ test/unit/sablier-v2-cliff/create-with-duration/createWithDuration.t.sol
@@ -6,5 +6,3 @@ import { ISablierV2Linear } from "@sablier/v2-core/interfaces/ISablierV2Linear.s
 import { stdError } from "forge-std/stdlib.sol";
 
 import { SablierV2UnitTest } from "../../SablierV2UnitTest.t.sol";
-
-/// See DSTestPlus solmate
