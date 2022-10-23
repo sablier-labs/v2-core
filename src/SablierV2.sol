8416a1abb6286618f889305e3114743207219684
andreivladbrg <andreivladbrg@gmail.com>
2022-10-24 00:03:41 +0300

    chore: remove unnecessary imports

--- src/SablierV2.sol
+++ src/SablierV2.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0
 pragma solidity >=0.8.13;
 
-import { IERC20 } from "@prb/contracts/token/erc20/IERC20.sol";
-import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
 import { ISablierV2 } from "./interfaces/ISablierV2.sol";
 
 /// @title SablierV2
