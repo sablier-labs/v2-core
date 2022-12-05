756733775c7e983887e339896bd54a32ffff3916
andreivladbrg <andreivladbrg@gmail.com>
2022-12-06 00:31:29 +0200

    chore: remove unused imports

--- src/libraries/Events.sol
+++ src/libraries/Events.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0
 pragma solidity >=0.8.13;
 
-import { IERC20 } from "@prb/contracts/token/erc20/IERC20.sol";
 import { SD59x18 } from "@prb/math/SD59x18.sol";
 
 /// @title Events
