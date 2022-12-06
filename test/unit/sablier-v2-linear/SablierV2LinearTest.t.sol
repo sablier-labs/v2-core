e577f13bb50a36fdae435b7671c5eb3e47e9df35
andreivladbrg <andreivladbrg@gmail.com>
2022-12-06 13:54:09 +0200

    test: implement "setUp" function in the unit tests

--- test/unit/sablier-v2-linear/SablierV2LinearTest.t.sol
+++ test/unit/sablier-v2-linear/SablierV2LinearTest.t.sol
@@ -3,8 +3,15 @@ pragma solidity >=0.8.13;
 
 import { UnitTest } from "../UnitTest.t.sol";
 
+import { SablierV2Linear } from "src/SablierV2Linear.sol";
+
 /// @title SablierV2LinearTest
 /// @notice Common contract members needed across SablierV2Linear unit tests
 abstract contract SablierV2LinearTest is UnitTest {
-    // solhint-disable-previous-line no-empty-blocks
+    SablierV2Linear internal sablierV2Linear;
+
+    /// @dev A setup function invoked before each test case.
+    function setUp() public virtual {
+        sablierV2Linear = new SablierV2Linear();
+    }
 }
--- test/unit/sablier-v2-pro/SablierV2ProTest.t.sol
+++ test/unit/sablier-v2-pro/SablierV2ProTest.t.sol
@@ -3,8 +3,15 @@ pragma solidity >=0.8.13;
 
 import { UnitTest } from "../UnitTest.t.sol";
 
+import { SablierV2Pro } from "src/SablierV2Pro.sol";
+
 /// @title SablierV2ProTest
-/// @notice Common contract members needed across SablierV2Linear unit tests
+/// @notice Common contract members needed across SablierV2Pro unit tests
 abstract contract SablierV2ProTest is UnitTest {
-    // solhint-disable-previous-line no-empty-blocks
+    SablierV2Pro internal sablierV2Pro;
+
+    /// @dev A setup function invoked before each test case.
+    function setUp() public virtual {
+        sablierV2Pro = new SablierV2Pro({ maxSegmentCount: MAX_SEGMENT_COUNT });
+    }
 }
