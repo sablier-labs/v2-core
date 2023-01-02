8b65d30b6daa09acce89452b584595651bb8de6e
Paul Razvan Berg <hello@paulrberg.com>
2023-01-02 22:15:36 +0200

    test: dryify "burn"

--- test/unit/linear/burn/burn.t.sol
+++ test/unit/linear/burn/burn.t.sol
@@ -1,86 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.13;
 
-import { Errors } from "src/libraries/Errors.sol";
+import { ISablierV2 } from "src/interfaces/ISablierV2.sol";
 
-import { LinearTest } from "../LinearTest.t.sol";
+import { Burn__Test } from "test/unit/shared/burn/Burn.t.sol";
+import { ProTest } from "test/unit/pro/ProTest.t.sol";
 
-contract Burn__Test is LinearTest {
-    uint256 internal defaultStreamId;
-
-    function setUp() public override {
-        LinearTest.setUp();
-
-        // Create the default stream, since most tests need it.
-        defaultStreamId = createDefaultStream();
-
-        // Make the owner of the NFT the caller in this test suite.
-        changePrank(users.recipient);
-    }
-
-    /// @dev it should revert.
-    function testCannotBurn__StreamExistent() external {
-        vm.expectRevert(abi.encodeWithSelector(Errors.SablierV2__StreamExistent.selector, defaultStreamId));
-        linear.burn(defaultStreamId);
-    }
-
-    modifier StreamNonExistent() {
-        _;
-    }
-
-    /// @dev it should revert.
-    function testCannotBurn__NFTNonExistent() external StreamNonExistent {
-        uint256 nonStreamId = 1729;
-        vm.expectRevert("ERC721: invalid token ID");
-        linear.burn(nonStreamId);
-    }
-
-    modifier NFTExistent() {
-        // Cancel the stream so that the stream entity gets deleted.
-        linear.cancel(defaultStreamId);
-        _;
-    }
-
-    /// @dev it should revert.
-    function testCannotBurn__CallerUnauthorized() external StreamNonExistent NFTExistent {
-        // Make Eve the caller in the rest of this test.
-        changePrank(users.eve);
-
-        // Run the test.
-        vm.expectRevert(abi.encodeWithSelector(Errors.SablierV2__Unauthorized.selector, defaultStreamId, users.eve));
-        linear.burn(defaultStreamId);
-    }
-
-    modifier CallerAuthorized() {
-        _;
-    }
-
-    /// @dev it should burn the NFT.
-    function testBurn__CallerApprovedOperator(
-        address operator
-    ) external StreamNonExistent NFTExistent CallerAuthorized {
-        vm.assume(operator != address(0) && operator != users.recipient);
-
-        // Approve the operator to handle the stream.
-        linear.approve({ to: operator, tokenId: defaultStreamId });
-
-        // Make the approved operator the caller in this test.
-        changePrank(operator);
-
-        // Burn the NFT.
-        linear.burn(defaultStreamId);
-
-        // Assert that the NFT was burned.
-        address actualOwner = linear.getRecipient(defaultStreamId);
-        address expectedOwner = address(0);
-        assertEq(actualOwner, expectedOwner);
-    }
-
-    /// @dev it should burn the NFT.
-    function testBurn__CallerNFTOwner() external StreamNonExistent NFTExistent CallerAuthorized {
-        linear.burn(defaultStreamId);
-        address actualOwner = linear.getRecipient(defaultStreamId);
-        address expectedOwner = address(0);
-        assertEq(actualOwner, expectedOwner);
+contract Burn__Pro__Test is ProTest, Burn__Test {
+    function setUp() public virtual override(ProTest, Burn__Test) {
+        Burn__Test.setUp();
+        sablierV2 = ISablierV2(pro);
     }
 }
---
+++ test/unit/shared/burn/burn.t.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.13;
+
+import { Errors } from "src/libraries/Errors.sol";
+
+import { SharedTest } from "../SharedTest.t.sol";
+
+abstract contract Burn__Test is SharedTest {
+    uint256 internal defaultStreamId;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        // Create the default stream, since most tests need it.
+        defaultStreamId = createDefaultStream();
+
+        // Make the owner of the NFT the caller in this test suite.
+        changePrank(users.recipient);
+    }
+
+    /// @dev it should revert.
+    function testCannotBurn__StreamExistent() external {
+        vm.expectRevert(abi.encodeWithSelector(Errors.SablierV2__StreamExistent.selector, defaultStreamId));
+        sablierV2.burn(defaultStreamId);
+    }
+
+    modifier StreamNonExistent() {
+        _;
+    }
+
+    /// @dev it should revert.
+    function testCannotBurn__NFTNonExistent() external StreamNonExistent {
+        uint256 nonStreamId = 1729;
+        vm.expectRevert("ERC721: invalid token ID");
+        sablierV2.burn(nonStreamId);
+    }
+
+    modifier NFTExistent() {
+        // Cancel the stream so that the stream entity gets deleted.
+        sablierV2.cancel(defaultStreamId);
+        _;
+    }
+
+    /// @dev it should revert.
+    function testCannotBurn__CallerUnauthorized() external StreamNonExistent NFTExistent {
+        // Make Eve the caller in the rest of this test.
+        changePrank(users.eve);
+
+        // Run the test.
+        vm.expectRevert(abi.encodeWithSelector(Errors.SablierV2__Unauthorized.selector, defaultStreamId, users.eve));
+        sablierV2.burn(defaultStreamId);
+    }
+
+    modifier CallerAuthorized() {
+        _;
+    }
+
+    /// @dev it should burn the NFT.
+    function testBurn__CallerApprovedOperator(
+        address operator
+    ) external StreamNonExistent NFTExistent CallerAuthorized {
+        vm.assume(operator != address(0) && operator != users.recipient);
+
+        // Approve the operator to handle the stream.
+        sablierV2.approve({ to: operator, tokenId: defaultStreamId });
+
+        // Make the approved operator the caller in this test.
+        changePrank(operator);
+
+        // Burn the NFT.
+        sablierV2.burn(defaultStreamId);
+
+        // Assert that the NFT was burned.
+        address actualOwner = sablierV2.getRecipient(defaultStreamId);
+        address expectedOwner = address(0);
+        assertEq(actualOwner, expectedOwner);
+    }
+
+    /// @dev it should burn the NFT.
+    function testBurn__CallerNFTOwner() external StreamNonExistent NFTExistent CallerAuthorized {
+        sablierV2.burn(defaultStreamId);
+        address actualOwner = sablierV2.getRecipient(defaultStreamId);
+        address expectedOwner = address(0);
+        assertEq(actualOwner, expectedOwner);
+    }
+}
