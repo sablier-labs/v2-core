7b3db7a5706ef7eda48037bbbe381a9495198c90
Paul Razvan Berg <hello@paulrberg.com>
2023-01-03 13:47:43 +0200

    feat: re-export token types

--- src/types/Math.sol
+++ src/types/Math.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0
 pragma solidity >=0.8.13;
 
-/// This file simply imports all PRBMath types needed in v2-core. It is provided for convenience
-/// to users so that they don't have to install PRBMath separately.
+/// This file simply re-exports all PRBMath types needed in v2-core. It is provided for convenience to
+/// users so that they don't have to install PRBMath separately.
 
 import { SD1x18 } from "@prb/math/SD1x18.sol";
 import { SD59x18 } from "@prb/math/SD59x18.sol";
