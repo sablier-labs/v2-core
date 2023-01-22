ec5b0e148bd8f812398f41188bdabb23c9899798
Paul Razvan Berg <hello@paulrberg.com>
2023-01-22 16:33:47 +0200

    perf: move withdrawn amount update in "if"

--- src/SablierV2Linear.sol
+++ src/SablierV2Linear.sol
@@ -322,13 +322,13 @@ contract SablierV2Linear is
         // Effects: mark the stream as canceled.
         _streams[streamId].status = Status.CANCELED;
 
-        // Effects: add the recipient's amount to the withdrawn amount.
-        unchecked {
-            _streams[streamId].amounts.withdrawn += recipientAmount;
-        }
-
-        // Interactions: withdraw the tokens to the recipient, if any.
         if (recipientAmount > 0) {
+            // Effects: add the recipient's amount to the withdrawn amount.
+            unchecked {
+                _streams[streamId].amounts.withdrawn += recipientAmount;
+            }
+
+            // Interactions: withdraw the tokens to the recipient.
             stream.token.safeTransfer({ to: recipient, amount: recipientAmount });
         }
 
--- src/SablierV2Pro.sol
+++ src/SablierV2Pro.sol
@@ -389,13 +389,13 @@ contract SablierV2Pro is
         // Effects: mark the stream as canceled.
         _streams[streamId].status = Status.CANCELED;
 
-        // Effects: add the recipient's amount to the withdrawn amount.
-        unchecked {
-            _streams[streamId].amounts.withdrawn += recipientAmount;
-        }
-
-        // Interactions: withdraw the tokens to the recipient, if any.
         if (recipientAmount > 0) {
+            // Effects: add the recipient's amount to the withdrawn amount.
+            unchecked {
+                _streams[streamId].amounts.withdrawn += recipientAmount;
+            }
+
+            // Interactions: withdraw the tokens to the recipient.
             stream.token.safeTransfer({ to: recipient, amount: recipientAmount });
         }
 
