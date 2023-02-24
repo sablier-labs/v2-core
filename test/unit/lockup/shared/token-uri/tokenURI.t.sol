4ab7bfdf9884c344b67aaa8d4109316ad976e688
andreivladbrg <andreivladbrg@gmail.com>
2023-02-24 12:14:45 +0200

    test: use capital letters for URI

--- test/unit/lockup/shared/token-uri/tokenURI.t.sol
+++ test/unit/lockup/shared/token-uri/tokenURI.t.sol
@@ -7,7 +7,7 @@ import { Unit_Test } from "../../../Unit.t.sol";
 abstract contract TokenURI_Unit_Test is Unit_Test, Lockup_Shared_Test {
     function setUp() public virtual override(Unit_Test, Lockup_Shared_Test) {}
 
-    /// @dev it should return the descriptor uri
+    /// @dev it should return the descriptor URI.
     function test_TokenURI_StreamNull() external {
         uint256 nullStreamId = 1729;
         string memory actualTokenURI = lockup.tokenURI({ tokenId: nullStreamId });
@@ -19,7 +19,7 @@ abstract contract TokenURI_Unit_Test is Unit_Test, Lockup_Shared_Test {
         _;
     }
 
-    /// @dev it should return the descriptor uri
+    /// @dev it should return the descriptor URI.
     function test_TokenURI() external streamNonNull {
         uint256 streamId = createDefaultStream();
         string memory actualTokenURI = lockup.tokenURI({ tokenId: streamId });
