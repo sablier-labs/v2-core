6767c59fd184c3f8e29ded38139b514c6b069f12
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-07 14:57:09 +0200

    test: remove directory "shared/helpers"

--- test/shared/mockups/flash-loan/GoodFlashLoanReceiver.t.sol
+++ test/shared/mockups/flash-loan/GoodFlashLoanReceiver.t.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.19 <0.9.0;
 import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol";
 import { IERC3156FlashBorrower } from "erc3156/interfaces/IERC3156FlashBorrower.sol";
 
-import { Constants } from "../../helpers/Constants.t.sol";
+import { Constants } from "../../Constants.t.sol";
 
 contract GoodFlashLoanReceiver is Constants, IERC3156FlashBorrower {
     function onFlashLoan(
