cde095f7eb2769608baf6cfb33793fa41816e14d
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-13 16:17:21 +0200

    refactor: use "IERC721Metadata" in "ISablierV2NFTDescriptor"

--- src/SablierV2NFTDescriptor.sol
+++ src/SablierV2NFTDescriptor.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.18;
 
-import { ISablierV2Lockup } from "./interfaces/ISablierV2Lockup.sol";
+import { IERC721Metadata } from "@openzeppelin/token/ERC721/extensions/IERC721Metadata.sol";
 import { ISablierV2NFTDescriptor } from "./interfaces/ISablierV2NFTDescriptor.sol";
 
 /// @title SablierV2NFTDescriptor
-/// @dev This is an example of an NFT descriptor, used in our scripts and tests.
+/// @dev This is a dummy NFT descriptor for demonstrational purposes.
 contract SablierV2NFTDescriptor is ISablierV2NFTDescriptor {
-    function tokenURI(ISablierV2Lockup lockup, uint256 streamId) external view override returns (string memory uri) {
-        lockup.getStartTime(streamId);
-        string memory symbol = lockup.symbol();
+    function tokenURI(IERC721Metadata sablierContract, uint256 streamId) external view override returns (string memory uri) {
+        streamId;
+        string memory symbol = sablierContract.symbol();
         uri = string.concat("This is the NFT descriptor for ", symbol);
     }
 }
--- src/interfaces/ISablierV2NFTDescriptor.sol
+++ src/interfaces/ISablierV2NFTDescriptor.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.18;
 
-import { ISablierV2Lockup } from "./ISablierV2Lockup.sol";
+import { IERC721Metadata } from "@openzeppelin/token/ERC721/extensions/IERC721Metadata.sol";
 
 /// @title ISablierV2NFTDescriptor
-/// @notice The contract that produces the URI describing the Sablier stream NFTs.
+/// @notice This contract produces the URI describing the Sablier stream NFTs.
 interface ISablierV2NFTDescriptor {
     /// @notice Produces the URI describing a particular stream NFT.
-    /// @dev Note This is a data URI with the JSON contents directly inlined.
-    /// @param lockup The address of the lockup streaming contract the stream belongs to.
+    /// @dev This is a data URI with the JSON contents directly inlined.
+    /// @param sablierContract The address of the Sablier contract the stream belongs to.
     /// @param streamId The id of the stream for which to produce a description.
     /// @return uri The URI of the ERC721-compliant metadata.
-    function tokenURI(ISablierV2Lockup lockup, uint256 streamId) external view returns (string memory uri);
+    function tokenURI(IERC721Metadata sablierContract, uint256 streamId) external view returns (string memory uri);
 }
