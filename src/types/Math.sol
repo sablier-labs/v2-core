a4d5ded8f43cb290abf9d86b5c34507d8f6a4339
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-03-16 16:39:16 +0200

    feat: export more PRBMath symbols

--- src/types/Math.sol
+++ src/types/Math.sol
@@ -4,6 +4,6 @@ pragma solidity >=0.8.13;
 // This file simply re-exports all PRBMath types needed in v2-core. It is provided for convenience so
 // that users don't have to install PRBMath separately.
 
-import { SD59x18 } from "@prb/math/SD59x18.sol";
-import { UD2x18 } from "@prb/math/UD2x18.sol";
-import { UD60x18 } from "@prb/math/UD60x18.sol";
+import { SD59x18, sd, sd59x18 } from "@prb/math/SD59x18.sol";
+import { UD2x18, ud2x18 } from "@prb/math/UD2x18.sol";
+import { UD60x18, ud, ud60x18 } from "@prb/math/UD60x18.sol";
