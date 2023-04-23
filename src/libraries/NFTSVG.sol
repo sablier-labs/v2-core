20d8a4016ba6e9904cf9965b563601285bd2410d
andreivladbrg <andreivladbrg@gmail.com>
2023-04-23 17:07:06 +0300

    feat: add status box in the SVG
    feat: add streamed amount abbreviation box
    feat: hash the chainid in the getColorAccent function
    feat: implement the half empty hourglass when the stream is depleted
    feat: add a bash script to extract the svg from tokenURI function
    fix: use the correct JSON string format
    refactor: remove the contract name box
    refactor: add the contract name in the rotating border
    refactor: reorder function parameters for improved clarity
    refactor: reorder struct parameters for improved clarity
    refactor: improve naming of the variables
    build: git ignore "tokenURI.svg"

--- src/libraries/NFTSVG.sol
+++ src/libraries/NFTSVG.sol
@@ -10,51 +10,62 @@ library NFTSVG {
     using Strings for uint256;
 
     struct GenerateParams {
-        string sablierContract;
-        string asset;
-        string assetSymbol;
-        string sablierContractType;
+        string colorAccent;
         string percentageStreamedString;
         uint256 percentageStreamedUInt;
+        string streamedAbbreviation;
         string durationInDays;
-        string colorAccent;
+        string sablierContract;
+        string sablierContractType;
+        string asset;
+        string assetSymbol;
         string recipient;
         string sender;
+        string status;
+        bool isDepleted;
     }
 
     struct GenerateVars {
-        string curveElement;
-        uint256 curveWidth;
         string progressElement;
         uint256 progressWidth;
+        uint256 progressLeftOffset;
+        string statusElement;
+        uint256 statusLeftOffset;
+        uint256 statusWidth;
+        string streamedElement;
+        uint256 streamedLeftOffset;
+        uint256 streamedWidth;
         string durationElement;
         uint256 durationWidth;
+        uint256 durationLeftOffset;
         uint256 row;
-        uint256 progressLeft;
-        uint256 curveLeft;
-        uint256 durationLeft;
     }
 
     function generate(GenerateParams memory params) internal pure returns (string memory) {
         GenerateVars memory vars;
-        (vars.curveElement, vars.curveWidth) =
-            SVGComponents.box(SVGComponents.BoxType.CURVE, params.sablierContractType, params.colorAccent, 0);
 
         (vars.progressElement, vars.progressWidth) = SVGComponents.box(
             SVGComponents.BoxType.PROGRESS,
-            params.percentageStreamedString,
             params.colorAccent,
+            params.percentageStreamedString,
             params.percentageStreamedUInt
         );
 
+        (vars.statusElement, vars.statusWidth) =
+            SVGComponents.box(SVGComponents.BoxType.STATUS, params.colorAccent, params.status, 0);
+
+        (vars.streamedElement, vars.streamedWidth) =
+            SVGComponents.box(SVGComponents.BoxType.STREAMED, params.colorAccent, params.streamedAbbreviation, 0);
+
         (vars.durationElement, vars.durationWidth) =
-            SVGComponents.box(SVGComponents.BoxType.DURATION, params.durationInDays, params.colorAccent, 0);
+            SVGComponents.box(SVGComponents.BoxType.DURATION, params.colorAccent, params.durationInDays, 0);
 
-        vars.row = vars.curveWidth + vars.progressWidth + vars.durationWidth + 20 * 2;
+        vars.row = vars.streamedWidth + vars.durationWidth + vars.progressWidth + vars.statusWidth + 20 * 3;
 
-        vars.progressLeft = (1000 - vars.row) / 2;
-        vars.curveLeft = vars.progressLeft + vars.progressWidth + 20;
-        vars.durationLeft = vars.curveLeft + vars.curveWidth + 20;
+        vars.progressLeftOffset = (1000 - vars.row) / 2;
+        vars.statusLeftOffset = vars.progressLeftOffset + vars.progressWidth + 20;
+        vars.streamedLeftOffset = vars.statusLeftOffset + vars.statusWidth + 20;
+        vars.durationLeftOffset = vars.streamedLeftOffset + vars.streamedWidth + 20;
 
         return Base64.encode(
             bytes(
@@ -62,13 +73,22 @@ library NFTSVG {
                     "data:application/json;base64,",
                     generateJSON(
                         JSONParams({
-                            generateVars: vars,
                             colorAccent: params.colorAccent,
+                            progressElement: vars.progressElement,
+                            progressLeftOffset: vars.progressLeftOffset.toString(),
+                            statusElement: vars.statusElement,
+                            statusLeftOffset: vars.statusLeftOffset.toString(),
+                            streamedElement: vars.streamedElement,
+                            streamedLeftOffset: vars.streamedLeftOffset.toString(),
+                            durationElement: vars.durationElement,
+                            durationLeftOffset: vars.durationLeftOffset.toString(),
                             sablierContract: params.sablierContract,
+                            sablierContractType: params.sablierContractType,
                             asset: params.asset,
                             assetSymbol: params.assetSymbol,
                             recipient: params.recipient,
-                            sender: params.sender
+                            sender: params.sender,
+                            isDepleted: params.isDepleted
                         })
                     )
                 )
@@ -77,28 +97,41 @@ library NFTSVG {
     }
 
     struct JSONParams {
-        GenerateVars generateVars;
         string colorAccent;
+        string progressElement;
+        string progressLeftOffset;
+        string statusElement;
+        string statusLeftOffset;
+        string streamedElement;
+        string streamedLeftOffset;
+        string durationElement;
+        string durationLeftOffset;
         string sablierContract;
+        string sablierContractType;
         string asset;
         string assetSymbol;
         string recipient;
         string sender;
+        bool isDepleted;
     }
 
     function generateJSON(JSONParams memory params) internal pure returns (string memory) {
         string memory finalSVG = generateSVG(
             SVGParams({
                 colorAccent: params.colorAccent,
-                curveElement: params.generateVars.curveElement,
-                progressElement: params.generateVars.progressElement,
-                durationElement: params.generateVars.durationElement,
+                progressElement: params.progressElement,
+                progressLeftOffset: params.progressLeftOffset,
+                statusElement: params.statusElement,
+                statusLeftOffset: params.statusLeftOffset,
+                streamedElement: params.streamedElement,
+                streamedLeftOffset: params.streamedLeftOffset,
+                durationElement: params.durationElement,
+                durationLeftOffset: params.durationLeftOffset,
                 sablierContract: params.sablierContract,
+                sablierContractType: params.sablierContractType,
                 asset: params.asset,
                 assetSymbol: params.assetSymbol,
-                progressLeft: params.generateVars.progressLeft.toString(),
-                curveLeft: params.generateVars.curveLeft.toString(),
-                durationLeft: params.generateVars.durationLeft.toString()
+                isDepleted: params.isDepleted
             })
         );
 
@@ -106,46 +139,62 @@ library NFTSVG {
         return string.concat(
             "{",
             '"name": "Sablier V2 NFT",',
-            '"description": This NFT represents a stream in SablierV2,',
-            "data:image/svg+xml;base64,",
+            '"description": "This NFT represents a stream in SablierV2",',
+            '"image": "data:image/svg+xml;base64,',
             Base64.encode(bytes(finalSVG)),
-            '"attributes": recipient: ',
+            '",',
+            '"attributes": { "recipient": "',
             params.recipient,
-            "sender: ",
+            '" , "sender": "',
             params.sender,
-            "}"
+            '"}}'
         );
     }
 
     struct SVGParams {
         string colorAccent;
-        string curveElement;
         string progressElement;
+        string progressLeftOffset;
+        string statusElement;
+        string statusLeftOffset;
+        string streamedElement;
+        string streamedLeftOffset;
         string durationElement;
+        string durationLeftOffset;
         string sablierContract;
+        string sablierContractType;
         string asset;
         string assetSymbol;
-        string progressLeft;
-        string curveLeft;
-        string durationLeft;
+        bool isDepleted;
     }
 
     function generateSVG(SVGParams memory params) internal pure returns (string memory) {
         return string.concat(
             '<svg height="1000" width="1000" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"> ',
-            generateDefs(params.colorAccent, params.curveElement, params.progressElement, params.durationElement),
+            generateDefs(
+                params.colorAccent,
+                params.progressElement,
+                params.statusElement,
+                params.streamedElement,
+                params.durationElement,
+                params.isDepleted
+            ),
             SVGComponents.BACKGROUND,
-            generateText(params.sablierContract, params.asset, params.assetSymbol),
-            generateHref(params.progressLeft, params.curveLeft, params.durationLeft),
+            generateText(params.sablierContract, params.sablierContractType, params.asset, params.assetSymbol),
+            generateHref(
+                params.progressLeftOffset, params.statusLeftOffset, params.streamedLeftOffset, params.durationLeftOffset
+            ),
             "</svg>"
         );
     }
 
     function generateDefs(
         string memory colorAccent,
-        string memory curveElement,
         string memory progressElement,
-        string memory durationElement
+        string memory statusElement,
+        string memory streamedElement,
+        string memory durationElement,
+        bool isDepleted
     )
         internal
         pure
@@ -155,37 +204,43 @@ library NFTSVG {
             "<defs>",
             SVGComponents.styles(colorAccent),
             SVGComponents.LIGHT,
-            SVGComponents.HOURGLASS,
+            SVGComponents.hourglass(isDepleted),
             SVGComponents.LOGO,
-            curveElement,
             progressElement,
+            statusElement,
+            streamedElement,
             durationElement,
             "</defs>"
         );
     }
 
     function generateHref(
-        string memory progressLeft,
-        string memory curveLeft,
-        string memory durationLeft
+        string memory progressLeftOffset,
+        string memory statusLeftOffset,
+        string memory streamedLeftOffset,
+        string memory durationLeftOffset
     )
         internal
         pure
         returns (string memory)
     {
         return string.concat(
-            '<use href="#hourglass" x="150" y="90" transform="rotate(10)" transform-origin="500 500" /> <use href="#progress" x="',
-            progressLeft,
-            '" y="800" /> <use href="#curve" x="',
-            curveLeft,
-            '" y="800" /> <use href="#duration" x="',
-            durationLeft,
-            '" y="800" />'
+            '<use href="#hourglass" x="150" y="90" transform="rotate(10)" transform-origin="500 500" />',
+            '<use href="#Progress" x="',
+            progressLeftOffset,
+            '" y="790" /> <use href="#Status" x="',
+            statusLeftOffset,
+            '" y="790" /> <use href="#Streamed" x="',
+            streamedLeftOffset,
+            '" y="790" /> <use href="#Duration" x="',
+            durationLeftOffset,
+            '" y="790" />'
         );
     }
 
     function generateText(
         string memory sablierContract,
+        string memory sablierContractType,
         string memory asset,
         string memory assetSymbol
     )
@@ -195,8 +250,8 @@ library NFTSVG {
     {
         return string.concat(
             '<text text-rendering="optimizeSpeed"> ',
-            SVGComponents.words("-100%", string.concat(sablierContract, " - Sablier")),
-            SVGComponents.words("0%", string.concat(sablierContract, " - Sablier")),
+            SVGComponents.words("-100%", string.concat(sablierContract, " - Sablier ", sablierContractType)),
+            SVGComponents.words("0%", string.concat(sablierContract, " - Sablier ", sablierContractType)),
             SVGComponents.words("-50%", string.concat(asset, " - ", assetSymbol)),
             SVGComponents.words("50%", string.concat(asset, " - ", assetSymbol)),
             "</text>"
