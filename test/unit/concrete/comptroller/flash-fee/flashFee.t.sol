5fc860036ffdca9c868c41a405ca2ceab922afc2
PraneshASP <praneshas2000@gmail.com>
2023-07-20 23:29:02 +0530

    test: update 'flashFee' unit-test to inherit from Comptroller

--- test/unit/concrete/comptroller/flash-fee/flashFee.t.sol
+++ test/unit/concrete/comptroller/flash-fee/flashFee.t.sol
@@ -3,19 +3,11 @@ pragma solidity >=0.8.19 <0.9.0;
 
 import { UD60x18, ZERO } from "@prb/math/UD60x18.sol";
 
-import { Base_Test } from "../../../../Base.t.sol";
+import { Comptroller_Unit_Concrete_Test } from "../Comptroller.t.sol";
 
-contract FlashFee_Unit_Concrete_Test is Base_Test {
-    /*//////////////////////////////////////////////////////////////////////////
-                                  SET-UP FUNCTION
-    //////////////////////////////////////////////////////////////////////////*/
-
+contract FlashFee_Unit_Concrete_Test is Comptroller_Unit_Concrete_Test {
     function setUp() public virtual override {
-        Base_Test.setUp();
-
-        // Deploy V2 Core.
-        deployCoreConditionally();
-
+        Comptroller_Unit_Concrete_Test.setUp();
         // Make the Admin the default caller in this test suite.
         vm.startPrank({ msgSender: users.admin });
     }
