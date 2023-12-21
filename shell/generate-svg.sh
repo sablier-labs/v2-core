c1a7fe555dda2bece15833930f1fed9dae7d4b2f
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-12-21 14:31:22 +0200

    chore: fix SVG generation script
    closes #764

--- shell/generate-svg.sh
+++ shell/generate-svg.sh
@@ -5,6 +5,7 @@
 
 # Pre-requisites:
 # - foundry (https://getfoundry.sh)
+# - sd (https://github.com/chmln/sd)
 
 # Strict mode: https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
 set -euo pipefail
@@ -24,11 +25,16 @@ output=$(
   "$arg_amount" \
   "$arg_duration"
 )
-svg=$(echo "$output" | awk -F "svg: string " '{print $2}' | awk 'NF > 0')
+
+# Forge adds 'svg: string ' as a prefix before the SVG
+# - The awk command records everything after the prefix, while filtering out empty lines
+# - `sd \\"` '"'` removes the escape backslashes
+# - `sd ^\"|\"$' ''` removes the starting and the ending double quotes
+svg=$(echo "$output" | awk -F "svg: string " '/svg: string /{print $2; exit}' |  sd '\\"' '"' | sd '^"|"$' '')
 
 # Generate the file name
 name="nft-${arg_progress}-${arg_status}-${arg_amount}-${arg_duration}.svg"
-sanitized="$(echo "$name" | sed "s/ //g" )" # remove whitespaces
+sanitized="$(echo "$name" | sd ' ' '' )" # remove whitespaces
 
 # Put the SVG in a file
 mkdir -p "out-svg"
