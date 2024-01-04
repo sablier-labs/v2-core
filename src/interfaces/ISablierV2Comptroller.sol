ec997ecde9bc2543fecdf4af91801f830fcc64f4
andreivladbrg <andreivladbrg@gmail.com>
2024-01-04 15:57:22 +0200

    feat: remove flash loan logic
    test: update tests accordingly
    docs: remove flash loan reference in README

--- src/interfaces/ISablierV2Comptroller.sol
+++ src/interfaces/ISablierV2Comptroller.sol
@@ -14,12 +14,6 @@ interface ISablierV2Comptroller is IAdminable {
                                        EVENTS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Emitted when the admin sets a new flash fee.
-    /// @param admin The address of the contract admin.
-    /// @param oldFlashFee The old flash fee, denoted as a fixed-point number.
-    /// @param newFlashFee The new flash fee, denoted as a fixed-point number.
-    event SetFlashFee(address indexed admin, UD60x18 oldFlashFee, UD60x18 newFlashFee);
-
     /// @notice Emitted when the admin sets a new protocol fee for the provided ERC-20 asset.
     /// @param admin The address of the contract admin.
     /// @param asset The contract address of the ERC-20 asset the new protocol fee has been set for.
@@ -27,27 +21,9 @@ interface ISablierV2Comptroller is IAdminable {
     /// @param newProtocolFee The new protocol fee, denoted as a fixed-point number.
     event SetProtocolFee(address indexed admin, IERC20 indexed asset, UD60x18 oldProtocolFee, UD60x18 newProtocolFee);
 
-    /// @notice Emitted when the admin enables or disables an ERC-20 asset for flash loaning.
-    /// @param admin The address of the contract admin.
-    /// @param asset The contract address of the ERC-20 asset to toggle.
-    /// @param newFlag Whether the ERC-20 asset can be flash loaned.
-    event ToggleFlashAsset(address indexed admin, IERC20 indexed asset, bool newFlag);
-
     /*//////////////////////////////////////////////////////////////////////////
                                  CONSTANT FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
-
-    /// @notice Retrieves the global flash fee, denoted as a fixed-point number where 1e18 is 100%.
-    ///
-    /// @dev Notes:
-    /// - This fee represents a percentage, not an amount. Do not confuse it with {IERC3156FlashLender.flashFee},
-    /// which calculates the fee amount for a specified flash loan amount.
-    /// - Unlike the protocol fee, this is a global fee applied to all flash loans, not a per-asset fee.
-    function flashFee() external view returns (UD60x18 fee);
-
-    /// @notice Retrieves a flag indicating whether the provided ERC-20 asset can be flash loaned.
-    /// @param token The contract address of the ERC-20 asset to check.
-    function isFlashAsset(IERC20 token) external view returns (bool result);
 
     /// @notice Retrieves the protocol fee for all streams created with the provided ERC-20 asset.
     /// @param asset The contract address of the ERC-20 asset to query.
@@ -57,19 +33,6 @@ interface ISablierV2Comptroller is IAdminable {
     /*//////////////////////////////////////////////////////////////////////////
                                NON-CONSTANT FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
-
-    /// @notice Updates the flash fee charged on all flash loans made with any ERC-20 asset.
-    ///
-    /// @dev Emits a {SetFlashFee} event.
-    ///
-    /// Notes:
-    /// - Does not revert if the fee is the same.
-    ///
-    /// Requirements:
-    /// - `msg.sender` must be the contract admin.
-    ///
-    /// @param newFlashFee The new flash fee to set, denoted as a fixed-point number where 1e18 is 100%.
-    function setFlashFee(UD60x18 newFlashFee) external;
 
     /// @notice Sets a new protocol fee that will be charged on all streams created with the provided ERC-20 asset.
     ///
@@ -86,14 +49,4 @@ interface ISablierV2Comptroller is IAdminable {
     /// @param asset The contract address of the ERC-20 asset to update the fee for.
     /// @param newProtocolFee The new protocol fee, denoted as a fixed-point number where 1e18 is 100%.
     function setProtocolFee(IERC20 asset, UD60x18 newProtocolFee) external;
-
-    /// @notice Toggles the flash loanability of an ERC-20 asset.
-    ///
-    /// @dev Emits a {ToggleFlashAsset} event.
-    ///
-    /// Requirements:
-    /// - `msg.sender` must be the admin.
-    ///
-    /// @param asset The address of the ERC-20 asset to toggle.
-    function toggleFlashAsset(IERC20 asset) external;
 }
--- test/integration/concrete/nft-descriptor/generateAccentColor.t.sol
+++ test/integration/concrete/nft-descriptor/generateAccentColor.t.sol
@@ -7,7 +7,7 @@ contract GenerateAccentColor_Integration_Concrete_Test is NFTDescriptor_Integrat
     function test_GenerateAccentColor() external {
         // Passing a dummy contract instead of a real Sablier contract to make this test easy to maintain.
         string memory actualColor = nftDescriptorMock.generateAccentColor_({ sablier: address(noop), streamId: 1337 });
-        string memory expectedColor = "hsl(302,69%,44%)";
+        string memory expectedColor = "hsl(268,54%,64%)";
         assertEq(actualColor, expectedColor, "accentColor");
     }
 }
