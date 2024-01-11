283e7b13597698961cbd1ca632bc63cc266979e3
andreivladbrg <andreivladbrg@gmail.com>
2024-01-11 16:44:58 +0200

    chore: use pragma solidity >=0.8.22

--- script/Init.s.sol
+++ script/Init.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.19 <0.9.0;
+pragma solidity >=0.8.22 <0.9.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ud2x18 } from "@prb/math/src/UD2x18.sol";
--- test/unit/concrete/nft-descriptor/hourglass.t.sol
+++ test/unit/concrete/nft-descriptor/hourglass.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.19 <0.9.0;
+pragma solidity >=0.8.22 <0.9.0;
 
 import { LibString } from "solady/src/utils/LibString.sol";
 
