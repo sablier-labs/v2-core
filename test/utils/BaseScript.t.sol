3f36df42e733e4a494bb238adcfc9aeeb502f5b4
andreivladbrg <andreivladbrg@gmail.com>
2024-02-12 16:00:27 +0200

    test: pragma solidity >=0.8.22 in base script test

--- test/utils/BaseScript.t.sol
+++ test/utils/BaseScript.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.19 <0.9.0;
+pragma solidity >=0.8.22 <0.9.0;
 
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { PRBTest } from "@prb/test/src/PRBTest.sol";
