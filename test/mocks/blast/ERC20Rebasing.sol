a2f14842cac935a972ae53ecbda43044fda0281f
smol-ninja <shubhamy2015@gmail.com>
2024-02-20 23:19:57 +0000

    test: add mock contracts to simulate Blast yield

--- test/mocks/blast/ERC20Rebasing.sol
+++ test/mocks/blast/ERC20Rebasing.sol
@@ -1,19 +1,41 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
+import { StdCheats } from "forge-std/src/StdCheats.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { PRBTest } from "@prb/test/src/PRBTest.sol";
 
 import { IERC20Rebasing } from "src/interfaces/Blast/IERC20Rebasing.sol";
 import { YieldMode } from "src/interfaces/Blast/IYield.sol";
 
-contract ERC20Rebasing is ERC20, IERC20Rebasing {
-    constructor() ERC20("Rebasing ERC20", "BERC20") { }
+contract ERC20Rebasing is ERC20, IERC20Rebasing, PRBTest, StdCheats {
+    mapping(address account => uint256) public claimableYield;
+    mapping(address account => YieldMode) public yieldMode;
 
-    function getClaimableAmount(address account) external view returns (uint256 amount) { }
+    constructor() ERC20("Rebasing ERC20", "BUSDToken") { }
 
-    function getConfiguration(address account) external view returns (YieldMode) { }
+    function getClaimableAmount(address account) public view returns (uint256 amount) {
+        return claimableYield[account];
+    }
 
-    function claim(address recipient, uint256 amount) external returns (uint256) { }
+    function getConfiguration(address account) public view returns (YieldMode) {
+        return yieldMode[account];
+    }
 
-    function configure(YieldMode yieldMode) external returns (uint256) { }
+    /// @dev This function is used to simulate claiming yield. It updates the `claimableYield` and sends the desired
+    /// amount to the recipient.
+    function claim(address recipient, uint256 amount) public returns (uint256) {
+        address account = msg.sender;
+        if (yieldMode[account] == YieldMode.CLAIMABLE) {
+            claimableYield[account] = claimableYield[account] - amount;
+            deal(address(this), recipient, balanceOf(recipient) + amount);
+            return amount;
+        }
+        return 0;
+    }
+
+    function configure(YieldMode yieldMode_) public returns (uint256) {
+        yieldMode[msg.sender] = yieldMode_;
+        return balanceOf(msg.sender);
+    }
 }
---
+++ test/mocks/blast/Gas.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+import { PRBTest } from "@prb/test/src/PRBTest.sol";
+
+import { GasMode, IGas } from "src/interfaces/Blast/IGas.sol";
+
+contract Gas is IGas, PRBTest {
+    /// @dev Blast.sol --> controls all access to Gas.sol
+    address public immutable blastConfigurationContract;
+
+    mapping(address contractAddress => uint256) public claimableGas;
+    mapping(address contractAddress => GasMode) public gasMode;
+
+    /// @dev this tracks the timestamp when gas is updated
+    uint256 public lastUpdated = block.timestamp;
+
+    /// @notice Allows only the Blast Configuration Contract to call a function
+    modifier onlyBlastConfigurationContract() {
+        require(msg.sender == blastConfigurationContract, "Caller must be blast configuration contract");
+        _;
+    }
+
+    constructor() {
+        blastConfigurationContract = msg.sender;
+    }
+
+    function readGasParams(address contractAddress)
+        public
+        view
+        returns (uint256 etherSeconds, uint256 etherBalance, uint256 lastUpdated_, GasMode gasMode_)
+    {
+        return (0, claimableGas[contractAddress], lastUpdated, GasMode.VOID);
+    }
+
+    /// @dev This function is used to simulate claiming gas.
+    function claim(
+        address contractAddress,
+        address recipient,
+        uint256 gasToClaim,
+        uint256
+    )
+        public
+        onlyBlastConfigurationContract
+        returns (uint256)
+    {
+        if (gasMode[contractAddress] == GasMode.CLAIMABLE) {
+            claimableGas[contractAddress] = claimableGas[contractAddress] - gasToClaim;
+            vm.deal(recipient, recipient.balance + gasToClaim);
+            return gasToClaim;
+        }
+
+        return 0;
+    }
+
+    /// @dev This function is used to simulate claiming all gas.
+    function claimAll(address contractAddress, address recipient) public returns (uint256) {
+        uint256 amount = claimableGas[contractAddress];
+        return claim(contractAddress, recipient, amount, 0);
+    }
+
+    function claimGasAtMinClaimRate(address contractAddress, address recipient, uint256) public returns (uint256) {
+        return claimAll(contractAddress, recipient);
+    }
+
+    function claimMax(address contractAddress, address recipient) public returns (uint256) {
+        return claimAll(contractAddress, recipient);
+    }
+
+    /// @dev This function is used to simulate configuring the gas mode of the `contractAddress`.
+    function setGasMode(address contractAddress, GasMode mode) public onlyBlastConfigurationContract {
+        gasMode[contractAddress] = mode;
+    }
+
+    function setLastUpdated() public onlyBlastConfigurationContract {
+        lastUpdated = block.timestamp;
+    }
+}
