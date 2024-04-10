259a055b268c48822861d6dc6155f0126a959bf7
smol-ninja <shubhamy2015@gmail.com>
2024-04-10 17:10:32 +0100

    test: NFTDescriptor compatibility test with v2.0

--- test/fork/NFTDescriptor.t.sol
+++ test/fork/NFTDescriptor.t.sol
@@ -16,14 +16,29 @@ abstract contract NFTDescriptor_Fork_Test is Fork_Test {
     constructor(IERC20 asset, address holder) Fork_Test(asset, holder) { }
 
     /*//////////////////////////////////////////////////////////////////////////
+                                      MODIFIERS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev Loads v2.0 pre-deployed on Mainnet.
+    modifier loadDeployments_V2_0() {
+        lockupDynamic = ISablierV2LockupDynamic(0x39EFdC3dbB57B2388CcC4bb40aC4CB1226Bc9E44);
+        lockupLinear = ISablierV2LockupLinear(0xB10daee1FCF62243aE27776D7a92D39dC8740f95);
+        _;
+    }
+
+    /// @dev Loads v2.1 pre-deployed on Mainnet.
+    modifier loadDeployments_V2_1() {
+        lockupDynamic = ISablierV2LockupDynamic(0x7CC7e125d83A581ff438608490Cc0f7bDff79127);
+        lockupLinear = ISablierV2LockupLinear(0xAFb979d9afAd1aD27C5eFf4E27226E3AB9e5dCC9);
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
                                   SET-UP FUNCTION
     //////////////////////////////////////////////////////////////////////////*/
 
     function setUp() public virtual override {
         Fork_Test.setUp();
-
-        // Loads lockup v2.1 pre-deployed contracts on Mainnet.
-        loadDeployments_V2_1();
     }
 
     /*//////////////////////////////////////////////////////////////////////////
@@ -32,7 +47,7 @@ abstract contract NFTDescriptor_Fork_Test is Fork_Test {
 
     /// @dev Given enough fuzz runs, all the following scenarios will be fuzzed:
     /// - Multiple values of streamId.
-    function testForkFuzz_TokenURI_LockupDynamic(uint256 streamId) external {
+    function testForkFuzz_TokenURI_LockupDynamic_V2_0(uint256 streamId) external loadDeployments_V2_0 {
         streamId = bound(streamId, 1, lockupDynamic.nextStreamId() - 1);
 
         string memory tokenURIBefore = lockupDynamic.tokenURI(streamId);
@@ -49,7 +64,24 @@ abstract contract NFTDescriptor_Fork_Test is Fork_Test {
 
     /// @dev Given enough fuzz runs, all the following scenarios will be fuzzed:
     /// - Multiple values of streamId.
-    function testForkFuzz_TokenURI_LockupLinear(uint256 streamId) external {
+    function testForkFuzz_TokenURI_LockupDynamic_V2_1(uint256 streamId) external loadDeployments_V2_1 {
+        streamId = bound(streamId, 1, lockupDynamic.nextStreamId() - 1);
+
+        string memory tokenURIBefore = lockupDynamic.tokenURI(streamId);
+
+        // Change the NFT descriptor for the previous versions of lockups to the newly deployed.
+        resetPrank({ msgSender: lockupDynamic.admin() });
+        lockupDynamic.setNFTDescriptor(nftDescriptor);
+
+        string memory tokenURIAfter = lockupDynamic.tokenURI(streamId);
+
+        // Assert that the token URI has not changed.
+        assertEq(tokenURIBefore, tokenURIAfter, "incompatible token URI");
+    }
+
+    /// @dev Given enough fuzz runs, all the following scenarios will be fuzzed:
+    /// - Multiple values of streamId.
+    function testForkFuzz_TokenURI_LockupLinear_V2_0(uint256 streamId) external loadDeployments_V2_0 {
         streamId = bound(streamId, 1, lockupLinear.nextStreamId() - 1);
 
         string memory tokenURIBefore = lockupLinear.tokenURI(streamId);
@@ -61,16 +93,23 @@ abstract contract NFTDescriptor_Fork_Test is Fork_Test {
         string memory tokenURIAfter = lockupLinear.tokenURI(streamId);
 
         // Assert that the token URI has not changed.
-        assertEq(tokenURIBefore, tokenURIAfter, "incompatible token URI incompatible");
+        assertEq(tokenURIBefore, tokenURIAfter, "incompatible token URI");
     }
 
-    /*//////////////////////////////////////////////////////////////////////////
-                                      HELPERS
-    //////////////////////////////////////////////////////////////////////////*/
+    /// @dev Given enough fuzz runs, all the following scenarios will be fuzzed:
+    /// - Multiple values of streamId.
+    function testForkFuzz_TokenURI_LockupLinear_V2_1(uint256 streamId) external loadDeployments_V2_1 {
+        streamId = bound(streamId, 1, lockupLinear.nextStreamId() - 1);
 
-    /// @dev Loads v2.1 pre-deployed on Mainnet.
-    function loadDeployments_V2_1() internal {
-        lockupDynamic = ISablierV2LockupDynamic(0x7CC7e125d83A581ff438608490Cc0f7bDff79127);
-        lockupLinear = ISablierV2LockupLinear(0xAFb979d9afAd1aD27C5eFf4E27226E3AB9e5dCC9);
+        string memory tokenURIBefore = lockupLinear.tokenURI(streamId);
+
+        // Change the NFT descriptor for the previous versions of lockups to the newly deployed.
+        resetPrank({ msgSender: lockupLinear.admin() });
+        lockupLinear.setNFTDescriptor(nftDescriptor);
+
+        string memory tokenURIAfter = lockupLinear.tokenURI(streamId);
+
+        // Assert that the token URI has not changed.
+        assertEq(tokenURIBefore, tokenURIAfter, "incompatible token URI");
     }
 }
