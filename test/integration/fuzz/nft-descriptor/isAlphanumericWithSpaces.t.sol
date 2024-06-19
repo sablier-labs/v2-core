94373aefc0c8d7c5374e215db67c9b34963d60ba
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2024-06-19 13:31:59 +0300

    refactor: rename alphanumeric function (#949)
    * refactor: rename alphanumeric function
    
    * chore: update precompiles
    
    ---------
    
    Co-authored-by: andreivladbrg <andreivladbrg@gmail.com>

--- test/integration/fuzz/nft-descriptor/isAlphanumericWithSpaces.t.sol
+++ test/integration/fuzz/nft-descriptor/isAlphanumericWithSpaces.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.22 <0.9.0;
 
 import { NFTDescriptor_Integration_Shared_Test } from "../../shared/nft-descriptor/NFTDescriptor.t.sol";
 
-contract IsAlphanumeric_Integration_Fuzz_Test is NFTDescriptor_Integration_Shared_Test {
+contract IsAlphanumericWithSpaces_Integration_Fuzz_Test is NFTDescriptor_Integration_Shared_Test {
     bytes1 internal constant SPACE = 0x20; // ASCII 32
     bytes1 internal constant ZERO = 0x30; // ASCII 48
     bytes1 internal constant NINE = 0x39; // ASCII 57
@@ -21,22 +21,22 @@ contract IsAlphanumeric_Integration_Fuzz_Test is NFTDescriptor_Integration_Share
     /// - String with only alphanumerical characters
     /// - String with only non-alphanumerical characters
     /// - String with both alphanumerical and non-alphanumerical characters
-    function testFuzz_IsAlphanumeric(string memory symbol) external view whenNotEmptyString {
+    function testFuzz_IsAlphanumericWithSpaces(string memory symbol) external view whenNotEmptyString {
         bytes memory b = bytes(symbol);
         uint256 length = b.length;
         bool expectedResult = true;
         for (uint256 i = 0; i < length; ++i) {
             bytes1 char = b[i];
-            if (!isAlphanumericChar(char)) {
+            if (!isAlphanumericOrSpaceChar(char)) {
                 expectedResult = false;
                 break;
             }
         }
-        bool actualResult = nftDescriptorMock.isAlphanumeric_(symbol);
-        assertEq(actualResult, expectedResult, "isAlphanumeric");
+        bool actualResult = nftDescriptorMock.isAlphanumericWithSpaces_(symbol);
+        assertEq(actualResult, expectedResult, "isAlphanumericWithSpaces");
     }
 
-    function isAlphanumericChar(bytes1 char) internal pure returns (bool) {
+    function isAlphanumericOrSpaceChar(bytes1 char) internal pure returns (bool) {
         bool isSpace = char == SPACE;
         bool isDigit = char >= ZERO && char <= NINE;
         bool isUppercaseLetter = char >= A && char <= Z;
