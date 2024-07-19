14bf8c9c41a8efb4a3f3eb9dcce9123545cd040b
andreivladbrg <andreivladbrg@gmail.com>
2024-07-19 14:41:20 +0300

    feat: add periphery contracts in src
    feat: add core dir in src
    refactor: remove redundant imports
    
    Co-authored-by: PaulRBerg <prberg@proton.me>
    Co-authored-by: smol-ninja <shubhamy2015@gmail.com>

--- script/DeployLockupTranched.s.sol
+++ script/DeployLockupTranched.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.22 <0.9.0;
 
-import { ISablierV2NFTDescriptor } from "../src/interfaces/ISablierV2NFTDescriptor.sol";
-import { SablierV2LockupTranched } from "../src/SablierV2LockupTranched.sol";
+import { ISablierV2NFTDescriptor } from "../src/core/interfaces/ISablierV2NFTDescriptor.sol";
+import { SablierV2LockupTranched } from "../src/core/SablierV2LockupTranched.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
