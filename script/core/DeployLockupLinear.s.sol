a829dbefef4e17062ca1c5934ca8c0ef0a4034cb
andreivladbrg <andreivladbrg@gmail.com>
2024-07-25 17:08:03 +0300

    feat: add periphery scripts
    feat: add core and periphery dirs under script
    feat: implement DeployDeterministicProtocol script
    
    refactor(shell): update CLI run in generate-svg script
    
    refactor(script): use count maps in deploy protocol
    
    chore: re-order imports
    
    Co-authored-by: PaulRBerg <prberg@proton.me>
    Co-authored-by: smol-ninja <shubhamy2015@gmail.com>

--- script/core/DeployLockupLinear.s.sol
+++ script/core/DeployLockupLinear.s.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.22 <0.9.0;
 
-import { ISablierV2NFTDescriptor } from "../src/core/interfaces/ISablierV2NFTDescriptor.sol";
-import { SablierV2LockupLinear } from "../src/core/SablierV2LockupLinear.sol";
+import { ISablierV2NFTDescriptor } from "../../src/core/interfaces/ISablierV2NFTDescriptor.sol";
+import { SablierV2LockupLinear } from "../../src/core/SablierV2LockupLinear.sol";
 
-import { BaseScript } from "./Base.s.sol";
+import { BaseScript } from "../Base.s.sol";
 
 contract DeployLockupLinear is BaseScript {
     function run(
