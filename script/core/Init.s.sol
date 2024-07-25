a248e61f08e5cfa7866f66929a97561e9532f9d2
andreivladbrg <andreivladbrg@gmail.com>
2024-07-25 17:08:03 +0300

    feat: add periphery scripts
    feat: add core and periphery dirs under script
    feat: implement DeployDeterministicProtocol script
    
    refactor(shell): update CLI run in generate-svg script
    
    refactor(script): use count maps in deploy protocol
    
    chore: re-order imports

--- script/core/Init.s.sol
+++ script/core/Init.s.sol
@@ -7,11 +7,11 @@ import { ud60x18 } from "@prb/math/src/UD60x18.sol";
 
 import { Solarray } from "solarray/src/Solarray.sol";
 
-import { ISablierV2LockupDynamic } from "../src/core/interfaces/ISablierV2LockupDynamic.sol";
-import { ISablierV2LockupLinear } from "../src/core/interfaces/ISablierV2LockupLinear.sol";
-import { Broker, LockupDynamic, LockupLinear } from "../src/core/types/DataTypes.sol";
+import { ISablierV2LockupDynamic } from "../../src/core/interfaces/ISablierV2LockupDynamic.sol";
+import { ISablierV2LockupLinear } from "../../src/core/interfaces/ISablierV2LockupLinear.sol";
+import { Broker, LockupDynamic, LockupLinear } from "../../src/core/types/DataTypes.sol";
 
-import { BaseScript } from "./Base.s.sol";
+import { BaseScript } from "../Base.s.sol";
 
 interface IERC20Mint {
     function mint(address beneficiary, uint256 value) external;
