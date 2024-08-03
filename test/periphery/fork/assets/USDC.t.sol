e153c4063dc7ff5037cbec949dd07d71c569aec5
smol-ninja <shubhamy2015@gmail.com>
2024-08-03 21:57:15 +0530

    test: refactor merkle-lockup folder to merkle-campaign
    Signed-off-by: smol-ninja <shubhamy2015@gmail.com>

--- test/periphery/fork/assets/USDC.t.sol
+++ test/periphery/fork/assets/USDC.t.sol
@@ -6,9 +6,9 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { CreateWithTimestamps_LockupDynamic_BatchLockup_Fork_Test } from "../batch-lockup/createWithTimestampsLD.t.sol";
 import { CreateWithTimestamps_LockupLinear_BatchLockup_Fork_Test } from "../batch-lockup/createWithTimestampsLL.t.sol";
 import { CreateWithTimestamps_LockupTranched_BatchLockup_Fork_Test } from "../batch-lockup/createWithTimestampsLT.t.sol";
-import { MerkleInstant_Fork_Test } from "../merkle/MerkleInstant.t.sol";
-import { MerkleLL_Fork_Test } from "../merkle/MerkleLL.t.sol";
-import { MerkleLT_Fork_Test } from "../merkle/MerkleLT.t.sol";
+import { MerkleInstant_Fork_Test } from "../merkle-campaign/MerkleInstant.t.sol";
+import { MerkleLL_Fork_Test } from "../merkle-campaign/MerkleLL.t.sol";
+import { MerkleLT_Fork_Test } from "../merkle-campaign/MerkleLT.t.sol";
 
 /// @dev An ERC-20 asset with 6 decimals.
 IERC20 constant usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
