b5156f28adf7834b699927537c935f9ed31f7c6d
andreivladbrg <andreivladbrg@gmail.com>
2024-09-09 03:26:58 +0300

    chore: use docs diagram

--- test/core/invariant/Lockup.t.sol
+++ test/core/invariant/Lockup.t.sol
@@ -261,7 +261,7 @@ abstract contract Lockup_Invariant_Test is Invariant_Test {
         }
     }
 
-    /// @dev See diagram at https://i.postimg.cc/sfHsBkWB/mermaid-diagram-2023-04-25-190035.png.
+    /// @dev See diagram at https://docs.sablier.com/concepts/protocol/statuses#diagram
     function invariant_StatusTransitions() external {
         uint256 lastStreamId = lockupStore.lastStreamId();
         if (lastStreamId == 0) {
