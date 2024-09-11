de0bce66dc1b7e3898e30f0695789dad8f75dff7
smol-ninja <shubhamy2015@gmail.com>
2024-09-11 17:29:19 +0100

    style: fix import

--- test/periphery/integration/merkle-campaign/instant/MerkleInstant.t.sol
+++ test/periphery/integration/merkle-campaign/instant/MerkleInstant.t.sol
@@ -2,13 +2,12 @@
 pragma solidity >=0.8.22 <0.9.0;
 
 import { ISablierMerkleBase } from "src/periphery/interfaces/ISablierMerkleBase.sol";
-
-import { Clawback_Integration_Test } from "../shared/clawback/clawback.t.sol";
-import { GetFirstClaimTime_Integration_Test } from "../shared/get-first-claim-time/getFirstClaimTime.t.sol";
-import { HasClaimed_Integration_Test } from "../shared/has-claimed/hasClaimed.t.sol";
-import { HasExpired_Integration_Test } from "../shared/has-expired/hasExpired.t.sol";
-import { WithdrawFees_Integration_Test } from "../shared/withdraw-fees/withdrawFees.t.sol";
-import { MerkleCampaign_Integration_Shared_Test } from "../shared/MerkleCampaign.t.sol";
+import { Clawback_Integration_Test } from "./../shared/clawback/clawback.t.sol";
+import { GetFirstClaimTime_Integration_Test } from "./../shared/get-first-claim-time/getFirstClaimTime.t.sol";
+import { HasClaimed_Integration_Test } from "./../shared/has-claimed/hasClaimed.t.sol";
+import { HasExpired_Integration_Test } from "./../shared/has-expired/hasExpired.t.sol";
+import { MerkleCampaign_Integration_Shared_Test } from "./../shared/MerkleCampaign.t.sol";
+import { WithdrawFees_Integration_Test } from "./../shared/withdraw-fees/withdrawFees.t.sol";
 
 /*//////////////////////////////////////////////////////////////////////////
                              NON-SHARED TESTS
