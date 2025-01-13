e427f7b11be4cabb6ba1ed017ba47c272725b0b0
Paul Razvan Berg <prberg@proton.me>
2025-01-13 14:25:10 +0200

    docs: separate assumptions

--- src/libraries/VestingMath.sol
+++ src/libraries/VestingMath.sol
@@ -129,7 +129,7 @@ library VestingMath {
     /// - $c$ is the cliff unlock amount.
     ///
     /// Assumptions:
-    /// 1. The sum of the unlock amounts (start and cliff) does not overflow uint128, and is less than or equal to
+    /// 1. The sum of the unlock amounts (start and cliff) does not overflow uint128 and is less than or equal to
     /// the deposit amount.
     /// 2. The start time is before the end time.
     /// 3. If the cliff time is not zero, it is after the start time and before the end time.
@@ -213,9 +213,9 @@ library VestingMath {
     ///
     /// Assumptions:
     /// 1. The sum of all tranche amounts does not overflow uint128, and equals the deposited amount.
-    /// 2. The first tranche's timestamp is greater than the start time, and the last tranche's timestamp
-    /// equals the end time.
-    /// 3. The tranche timestamps are arranged in ascending order.
+    /// 2. The first tranche's timestamp is greater than the start time.
+    /// 3. The last tranche's timestamp equals the end time.
+    /// 4. The tranche timestamps are arranged in ascending order.
     function calculateLockupTranchedStreamedAmount(
         uint128 depositedAmount,
         uint40 blockTimestamp,
